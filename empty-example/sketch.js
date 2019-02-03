2db5e93a0403c541c3f0850db0fd1bdf33298782
Andrew Kou <andrewhkou@gmail.com>
2019-02-03 14:53:39 -0800

    first commit

---
+++ empty-example/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  // put setup code here
+}
+
+let value  = 0;
+
+function draw() {
+  // put drawing code here
+  fill(value);
+  ellipse(25, 25, 50, 50);
+}
+
+function mouseClicked() {
+  if (value === 0) {
+    value = random(0, 250);
+  } else {
+    value = random(0, 250);
+  }
+}
